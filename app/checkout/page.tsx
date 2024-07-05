d0128caf7ac1a27b8ae964d876effe3d8d548578
Web Apps for Industry <info@webappsforindustry.tech>
2024-07-05 17:58:58 +0200

    initial commit

---
+++ app/checkout/page.tsx
@@ -0,0 +1,5 @@
+function CheckoutPage() {
+  return <h1 className="text-3xl">CheckoutPage</h1>;
+}
+
+export default CheckoutPage;
---
+++ app/favorites/page.tsx
@@ -0,0 +1,5 @@
+function FavoritesPage() {
+  return <h1 className="text-3xl">FavoritesPage</h1>;
+}
+
+export default FavoritesPage;
