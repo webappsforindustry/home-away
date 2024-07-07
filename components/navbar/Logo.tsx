a095b2e5b77835d476dd944f5b7472cf5d2ff692
Web Apps for Industry <info@webappsforindustry.tech>
2024-07-07 13:52:02 +0200

    add shadcn stories

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,4 +1,17 @@
+import Link from 'next/link';
+import { LuTent } from 'react-icons/lu';
+import { Button } from '../ui/button';
+
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Button
+      size="icon"
+      asChild
+    >
+      <Link href="/">
+        <LuTent className='w-6 h-6' />
+      </Link>
+    </Button>
+  );
 }
 export default Logo;
